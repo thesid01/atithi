c2014a2436ece8655f44bbfebe127062e365e585
srijansingh53 <srijansingh53@gmail.com>
2020-03-10 18:54:44 +0530

    test done

---
+++ __init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""This module contains the MindMeld HR assistant blueprint application"""
+
+from test_app.root import app
+
+import test_app.greeting
+import test_app.general
+import test_app.faq
+
+__all__ = ['app']
---
+++ greeting.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""This module contains the dialogue states for the 'greeting' domain in
+the atithi application
+"""
+
+from .root import app
+
+
+@app.handle(intent='greet')
+def greet(request, responder):
+    responder.reply("Namaskar Atithi, What kind of tour would you like to go on today?")
+
+
+@app.handle(intent='exit')
+def exit(request, responder):
+    responder.reply('Alright, See you soon')
