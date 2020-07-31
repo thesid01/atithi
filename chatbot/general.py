52abea697d190f669007f07069180a9331a84f1a
Siddharth <sidkv.skv@gmail.com>
2020-07-31 14:17:31 +0530

    fixed issue

--- chatbot/general.py
+++ chatbot/general.py
@@ -4,6 +4,30 @@ import json
 from .helpers import extract_entities_from_type
 
 NOT_A_CITY="Sorry😕...This city is not in India."+"~"+"Let me help you with this🙂"+"~"+"Some popular cities in India are : Lucknow, Goa, Jodhpur,  etc."+"~"+"Please choose another city."
+NOT_A_SPOT="Sorry😕...This spot is not in our list."+"~"+"Let me help you with this🙂"+"~"+"Some popular spots in India are : Tarsar Marsar, Nandi Hills, , Spiti Valley etc."+"~"+"Please choose another spot."
+
+
+@app.handle(intent = 'get_spotinfo', has_entity='spot_name')
+def get_spot_info(request, responder):
+    try :
+        spot_name = "None"
+        if request.entities[0]["type"] == "spot_name":
+            spot_name = request.entities[0]["value"][0]["cname"]
+        else:
+            spot_name = request.entities[1]["value"][0]["cname"]
+        data = app.question_answerer.get(index='spot_data', spot_name=spot_name)
+        trivia= ""
+        try:
+            trivia = data[0]["trivia"]
+        except KeyError:
+            trivia = "Trivia is empty now"
+        image_URL = data[0]["image_URL"]
+        data = image_URL + "~" + trivia
+        responder.reply(data)
+    except:
+        responder.reply(NOT_A_SPOT)
+
+
 @app.handle(domain='general', intent='getcity_intro')
 def get_city(request, responder):
     name = request.frame.get('city_name')
@@ -21,21 +45,21 @@ def get_city(request, responder):
         responder = _fetch_from_kb(responder, name, entity_type)
     return responder
 
-@app.handle(domain='general',intent='change_pref')
-def change_hotel_pref(request,responder):
-    responder.params.allowed_intents = ('general.change_hotel_pref','general.change_food_pref')
-    responder.reply('Sure, which preference do you want to change- hotel or food')
+# @app.handle(domain='general',intent='change_pref')
+# def change_hotel_pref(request,responder):
+#     responder.params.allowed_intents = ('general.change_hotel_pref','general.change_food_pref')
+#     responder.reply('Sure, which preference do you want to change- hotel or food')
 
-@app.handle(domain='general',intent='change_hotel_pref')
-def change_hotel_pref(request,responder):
-    responder.params.allowed_intents = ('tourism.hotel_pref')
-    responder.reply("Fine, now please tell us any preferences for hotels i.e Number of rooms ac/non-ac/etc.")
+# @app.handle(domain='general',intent='change_hotel_pref')
+# def change_hotel_pref(request,responder):
+#     responder.params.allowed_intents = ('tourism.hotel_pref')
+#     responder.reply("Fine, now please tell us any preferences for hotels i.e Number of rooms ac/non-ac/etc.")
     
 
-@app.handle(domain='general',intent='change_food_pref')
-def change_hotel_pref(request,responder):
-    responder.params.allowed_intents = ('tourism.food_pref')
-    responder.reply("Fine, now please tell us any preferences about your food (veg/non-veg/italian/etc)")
+# @app.handle(domain='general',intent='change_food_pref')
+# def change_hotel_pref(request,responder):
+#     responder.params.allowed_intents = ('tourism.food_pref')
+#     responder.reply("Fine, now please tell us any preferences about your food (veg/non-veg/italian/etc)")
 
 def _fetch_from_kb(responder, name, entity_type):
     """
