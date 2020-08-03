79ef78e759f2803d940e53ddde8ce90fee6b629e
Rohan Saxena <freak.rhn@gmail.com>
2020-08-03 10:33:03 +0530

    user suggestions

--- chatbot/helpers.py
+++ chatbot/helpers.py
@@ -1,2 +1,6 @@
 def extract_entities_from_type(request, entity_type):
     return [e for e in request.entities if e['type'] == entity_type]
+
+def _fetch_general_user_suggestion():
+    suggestion = app.question_answerer.get(index='user_suggestions')
+    return suggestion[random.randrange(0,len(suggestion))]
