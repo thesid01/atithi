2c2e085e450f693e05cbf166587dc6a620bd9161
Siddharth Kumar Verma <sidkv.skv@outlook.com>
2020-08-08 17:06:44 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,5 +1,5 @@
 cd ./chatbot && ./load_kb.sh && cd ..
 python -m chatbot build
-export TWILIO_AUTH_TOKEN=6e41fd5c5be903def94f9e7efaaa15ed
-export TWILIO_ACCOUNT_SID=AC090860c33f406130a009592dbd376709
+export TWILIO_AUTH_TOKEN=
+export TWILIO_ACCOUNT_SID=
 python whatsapp_bot_server.py
