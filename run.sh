6ada35d1eb2242297f1a91b7e0a32c06a6d7a51c
Siddharth Kumar Verma <sidkv.skv@outlook.com>
2020-08-08 17:06:22 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-export TWILIO_AUTH_TOKEN=6e41fd5c5be903def94f9e7efaaa15ed
-export TWILIO_ACCOUNT_SID=AC090860c33f406130a009592dbd376709
+export TWILIO_AUTH_TOKEN=
+export TWILIO_ACCOUNT_SID=
 python whatsapp_bot_server.py
